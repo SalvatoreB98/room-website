50d3f74ed5fb9930b3c7dbc39527b516b6066378
Salvatore Butera <salvatore.butera@nttdata.com>
2023-01-27 18:59:18 +0100

    EMail testing

--- main.js
+++ main.js
@@ -2,12 +2,24 @@ import './style.css';
 import './tagStyle.css';
 import './styleDarkTheme.css';
 import Experience from './Experience/Experience.js';
-
+import emailjs from '@emailjs/browser';
 const experience = new Experience(document.querySelector(".experience-canvas"));
 
 window.addEventListener("load",()=>{
+    emailjs.init("bqrEkrXPg6tVYWgdR")
     var button = document.querySelector("[type=submit]")
     button.addEventListener("click", (e)=>{
         e.preventDefault();
+        var params = {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            message: document.getElementById("message").value
+        }
+        emailjs.send(serviceID, templateID, params).then(res=>{
+            console.warn(res)
+        })
     })
 })
+
+const serviceID="service_0whlgnp";
+const templateID="template_cbmm5hf";
