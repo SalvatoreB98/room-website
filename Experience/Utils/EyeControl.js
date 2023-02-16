38c714304fe98d0a436c94c363cb79e1299b15ac
Salvatore Butera <salvatore.butera@nttdata.com>
2023-02-16 20:37:16 +0100

    fix & update

--- Experience/Utils/EyeControl.js
+++ Experience/Utils/EyeControl.js
@@ -3,13 +3,26 @@ export default class EyeControl{
     constructor(){
         this.experience = new Experience();
         this.sizes = this.experience.sizes;
+        this.pupils = []
+        this.eyes = document.querySelectorAll(".eye")
         window.addEventListener("mousemove",(e)=>{
-            var pupils = document.querySelectorAll(".pupil");
-            pupils.forEach(pupil=>{
+            this.pupils = document.querySelectorAll(".pupil");
+            this.pupils.forEach(pupil=>{
                 console.log(e, this.sizes)
                 pupil.style.top = e.clientY * 60 / this.sizes.height + "%";
                 pupil.style.left = e.clientX * 50 / this.sizes.width + "%";
             })
+        })
+        window.addEventListener("keydown",(e)=>{
+            if(e.code == "Space"){
+                this.eyes.forEach(eye=>{
+                    console.log("BLINK")
+                    eye.style.height = "2px  ";
+                    setTimeout(()=>{
+                        eye.style.height = "1em";
+                    },100)
+                })
+            }
         })
     }
 }
