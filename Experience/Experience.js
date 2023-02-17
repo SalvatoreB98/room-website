94247c0a625dad24f25c0863d8968dc79611a9bb
Salvatore Butera <salvatore.butera@nttdata.com>
2023-02-17 02:15:07 +0100

    Audio OK

--- Experience/Experience.js
+++ Experience/Experience.js
@@ -10,6 +10,7 @@ import Lang from './Utils/Lang';
 import Animations from './Utils/animations.js'
 import Theme from './Theme';
 import Preloader from "./Preloader.js";
+import Music from "./Utils/Music.js";
 export default class Experience {
     static instance;
     constructor(canvas) {
@@ -30,6 +31,7 @@ export default class Experience {
         this.animations = new Animations();
         this.world = new World();
         this.preloader = new Preloader();
+        this.music = new Music();
         this.time.update();
         
         this.time.on("update", () => {
--- Experience/Utils/Music.js
+++ Experience/Utils/Music.js
@@ -1,7 +1,81 @@
 import Experience from "../Experience";
+import music1 from "../../src/sounds/music1.wav"
+import music2 from "../../src/sounds/music2.wav"
+import music3 from "../../src/sounds/music3.wav"
+import music4 from "../../src/sounds/music4.wav"
+import music5 from "../../src/sounds/music5.wav"
+import music6 from "../../src/sounds/music6.wav"
+import Controls from "../World/Controls";
 export default class Music{
     constructor(){
+        this.scrollTop;
         this.experience = new Experience();
         this.sizes = this.experience.sizes;
+        this.world = this.experience.world;
+        this.page = document.querySelector(".page");
+        this.isMusicPlaying = false;
+        this.instruments = {
+            kick: new Audio(music1),
+            kasio: new Audio(music2),
+            perc: new Audio(music3),
+            bass: new Audio(music4),
+            bass2: new Audio(music5),
+            chord: new Audio(music6),
+        }
+        document.addEventListener("click", () => {
+            this.playMusic();
+        });
+        window.addEventListener("scroll",()=>{
+            this.changepx = this.page.clientHeight / 12;
+            this.scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+            var scrolled = this.scrollTop / this.changepx;
+            if(scrolled > 0 && scrolled < 1){
+                // PRIMO STEP AUDIO
+                console.log("step1")
+                this.instrumentsToPlay(["kasio"])
+            } else if(scrolled > 1  && scrolled < 2){
+                // SECONDO STEP AUDIO
+                console.log("step2")
+                this.instrumentsToPlay(["kasio","perc"])
+            } else if(scrolled > 2  && scrolled < 3){
+                // TERZO STEP AUDIO
+                console.log("step3")
+                this.instrumentsToPlay(["kasio","perc", "kick"])
+            } else if(scrolled > 3  && scrolled < 4){
+                // QUARTO STEP AUDIO
+                console.log("step4")
+                this.instrumentsToPlay(["kasio","perc", "kick", "bass"])
+            } else if(scrolled > 4  && scrolled < 5){
+                //QUINTO STEP AUDIO
+                console.log("step5")
+                this.instrumentsToPlay(["kasio","perc", "kick", "bass", "chord"])
+            } else if(scrolled > 5){
+                // SESTO STEP AUDIO
+                console.log("step6")
+                this.instrumentsToPlay(["kasio","perc", "kick", "bass", "chord", "bass2"])
+            }
+        })
+    }
+    playMusic(){
+        if(!this.isMusicPlaying){
+            for (const [key, audio] of Object.entries(this.instruments)) {
+                audio.play();
+                audio.loop = true;
+                audio.volume = 0;
+                if(key == "kasio"){
+                    audio.volume = 1
+                }
+            }
+            this.isMusicPlaying = true;
+        }
+    }
+    instrumentsToPlay(intrumentKeys){
+        for (const [key, audio] of Object.entries(this.instruments)) {
+            if(intrumentKeys.includes(key)){
+                audio.volume = 1
+            } else{
+                audio.volume = 0
+            }
+        }
     }
 }
